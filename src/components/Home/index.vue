d4b25f56c461ff27cce43ce990c2a469702b9c10
王俊峰 <wangjunfeng@bianxianmao.com>
2020-12-17 17:55:46 +0800

    login

---
+++ src/components/Home/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <el-button type="info" @click="loginOut">退出</el-button>
+  </div>
+</template>
+<script>
+export default {
+    methods:{
+        loginOut() {
+            this.$router.push({path:'/login'});
+            window.sessionStorage.removeItem('token');
+        }
+    }
+}
+</script>
+<style lang="less" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css'; 
+import './assets/css/global.css'
+
+import './assets/font/iconfont.css'
+
+import axios from 'axios'
+axios.defaults.baseURL="https://www.liulongbin.top:8888/api/private/v1/";
+Vue.prototype.$http = axios;
+ 
+Vue.use(ElementUI);  
 
 Vue.config.productionTip = false
 
