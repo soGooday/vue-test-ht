c8e65882e84d5760ba199ca0172250e0610b4b92
王俊峰 <wangjunfeng@bianxianmao.com>
2021-01-17 12:56:04 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -10,6 +10,12 @@ import './assets/font/iconfont.css'
 
 import axios from 'axios'
 axios.defaults.baseURL="https://www.liulongbin.top:8888/api/private/v1/";
+axios.interceptors.request.use(config=>{ 
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config;
+
+})
+
 Vue.prototype.$http = axios;
  
 Vue.use(ElementUI);  
